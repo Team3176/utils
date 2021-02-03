6d1b0c199644f1cc84e57a36bf0eab9baf8dcbd6
Kyle <45471789+kphillips2003@users.noreply.github.com>
2021-02-03 08:22:42 -0500

    Made some changes but need to finish in VS code

--- Compressor/src/main/java/frc/robot/Robot.java
+++ Compressor/src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2020 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Compressor;
@@ -12,6 +5,8 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import double solenoid
+//import double solenoid value
 
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
@@ -20,6 +15,7 @@ public class Robot extends TimedRobot {
 
   Compressor c = new Compressor();
   Joystick joy = new Joystick(0);
+//create 4 double solenoids and leave notes
 
   @Override
   public void robotInit() {
@@ -49,6 +45,7 @@ public class Robot extends TimedRobot {
     if(joy.getRawButton(1)) {
       c.stop();
     }
+//add a start compressor, add buttons to test each of the 4 double solenoids
   }
 
   @Override
